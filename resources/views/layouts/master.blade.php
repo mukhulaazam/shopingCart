203e1c0fee1febffbba055bf4099680402167563
mukhulaazam <mukhulaazam@gmail.com>
2018-08-27 23:47:09 +0600

    Product iteam design compeleted next is full design and database

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,17 +7,22 @@
     <title>@yield('title')</title>
     
     <!-- StyleSheets -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Font Awesome Icon -->
     @yield('styles')
 </head>
 <body>
     @include('partials.header')
-    @yield('content')
+
+    <div class="container">
+        @yield('content')
+    </div>
     
     <!-- JavaScripts -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     @yield('scripts')
 </body>
 </html>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -3,5 +3,16 @@
     Home
     @endsection
 @section('content')
-<h1>Hello ! Mamun this pages working</h1>
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="thumbnail">
+                <img src=". . ." alt="...">
+                <div class="caption">
+                    <h3>Product Title</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nihil optio reiciendis vero eaque distinctio pariatur excepturi animi consectetur. Dicta, saepe. Ex recusandae itaque optio similique quisquam earum, dolores vero.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Button</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
