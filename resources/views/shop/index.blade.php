7232afb61e0ecca6601e6177ddba20792ccae58f
mukhulaazam <mukhulaazam@gmail.com>
2018-08-30 12:05:45 +0600

    All data comes from databasa dynamically and next is sign in page

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -3,106 +3,25 @@
     Home
     @endsection
 @section('content')
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-            <img src="http://ecx.images-amazon.com/images/I/919-FLL37TL.jpg" alt="" srcset="">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nihil optio
-                     reiciendis vero eaque distinctio pariatur excepturi animi consectetur. Dicta,
-                      saepe. Ex recusandae itaque optio similique quisquam earum, dolores vero.
-                    </p>
-                    <div class="clearfix">
-                    <p class="pull-left price">$15</p>
-                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+    @foreach($products->chunk(3) as $productChunk)
+        <div class="row">
+        @foreach($productChunk as $product)
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                <img src="{{$product->imagePath}}" alt="" srcset="">
+                    <div class="caption">
+                        <h3>{{$product->title}}</h3>
+                        <p class="description">
+                        {{$product->description}}
+                        </p>
+                        <div class="clearfix">
+                        <p class="pull-left price">${{$product->price}}</p>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                        </div>
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-            <img src="http://ecx.images-amazon.com/images/I/919-FLL37TL.jpg" alt="" srcset="">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nihil optio
-                     reiciendis vero eaque distinctio pariatur excepturi animi consectetur. Dicta,
-                      saepe. Ex recusandae itaque optio similique quisquam earum, dolores vero.
-                    </p>
-                    <div class="clearfix">
-                    <p class="pull-left price">$15</p>
-                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-            <img src="http://ecx.images-amazon.com/images/I/919-FLL37TL.jpg" alt="" srcset="">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nihil optio
-                     reiciendis vero eaque distinctio pariatur excepturi animi consectetur. Dicta,
-                      saepe. Ex recusandae itaque optio similique quisquam earum, dolores vero.
-                    </p>
-                    <div class="clearfix">
-                    <p class="pull-left price">$15</p>
-                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- asdfghjkltyuijcvhbnjtvyhunj -->
-
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-            <img src="http://ecx.images-amazon.com/images/I/919-FLL37TL.jpg" alt="" srcset="">>
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nihil optio
-                     reiciendis vero eaque distinctio pariatur excepturi animi consectetur. Dicta,
-                      saepe. Ex recusandae itaque optio similique quisquam earum, dolores vero.
-                    </p>
-                    <div class="clearfix">
-                    <p class="pull-left price">$15</p>
-                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-            <img src="http://ecx.images-amazon.com/images/I/919-FLL37TL.jpg" alt="" srcset="">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nihil optio
-                     reiciendis vero eaque distinctio pariatur excepturi animi consectetur. Dicta,
-                      saepe. Ex recusandae itaque optio similique quisquam earum, dolores vero.
-                    </p>
-                    <div class="clearfix">
-                    <p class="pull-left price">$15</p>
-                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <!-- <img src="{{URL::asset('/images/apple.jpg')}}" alt="..."> -->
-                <img src="http://ecx.images-amazon.com/images/I/919-FLL37TL.jpg" alt="" srcset="">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nihil optio
-                     reiciendis vero eaque distinctio pariatur excepturi animi consectetur. Dicta,
-                      saepe. Ex recusandae itaque optio similique quisquam earum, dolores vero.
-                    </p>
-                    <div class="clearfix">
-                    <p class="pull-left price">$15</p>
-                    <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
+    @endforeach
 @endsection
